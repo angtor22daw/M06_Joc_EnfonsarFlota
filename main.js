ba04534e13138066a82f47853712e2a492f09cd4
polsan22daw <polsan22daw@proton.me>
2023-03-24 16:29:32 +0100

    2 taules

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 window.onload = () => {
     crearTabla();
+    crearTaula2();
 }
 function crearTabla() {
     // Creamos la tabla
@@ -36,8 +37,54 @@ function crearTabla() {
         table.appendChild(fila);
     }
 
+    var celda = table.rows[0].cells[0];
+    celda.style.visibility = "hidden";
+
     // Agregamos la tabla al div
     var divTabla = document.getElementById("mydiv");
+    divTabla.appendChild(table);
+}
+
+function crearTaula2() {
+    // Creamos la tabla
+    var table = document.createElement("table");
+
+    // Creamos las demás filas
+    for (var tdColumna = 0; tdColumna < 11; tdColumna++) {
+        // si es la primera columna, la enumeramos
+        var fila = document.createElement("tr");
+        if(tdColumna == 0){
+            var celdaNumerica = document.createElement("td");
+            celdaNumerica.textContent = "";
+            fila.appendChild(celdaNumerica);   
+        }else{
+            var letra = String.fromCharCode(64 + tdColumna);
+            var celdaNumerica = document.createElement("td");
+            celdaNumerica.textContent = letra;
+            fila.appendChild(celdaNumerica);
+        }
+
+        // Creamos las demás celdas vacías
+        for (var tdFila = 1; tdFila < 11; tdFila++) {
+            if (tdColumna == 0) {
+            var celdaVacia = document.createElement("td");
+            celdaVacia.textContent = tdFila;
+            fila.appendChild(celdaVacia);
+
+            } else {
+            var celdaVacia = document.createElement("td");
+            fila.appendChild(celdaVacia);
+            }
+        }
+
+        table.appendChild(fila);
+    }
+
+    var celda = table.rows[0].cells[0];
+    celda.style.visibility = "hidden";
+
+    // Agregamos la tabla al div
+    var divTabla = document.getElementById("taula2");
     divTabla.appendChild(table);
 }
 
